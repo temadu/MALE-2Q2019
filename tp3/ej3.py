c9da36b23eca37fb8f9984887974d498934f0ca5
Ezequiel Lynch <ezeqlynch2@gmail.com>
2019-10-16 04:08:37 -0300

    change vaca thing

--- tp3/ej3.py
+++ tp3/ej3.py
@@ -30,11 +30,10 @@ def loadTestingFileLabels(filepath):
     return set
 
 
-def imageResult(predictions):
+def imageResult(predictions, kernel, c, rows, cols):
     # Create a 1024x1024x3 array of 8 bit unsigned integers
-    rows = 1140
-    cols = 760
-    data = np.zeros((760, 1140, 3), dtype=np.uint8)
+    data = np.zeros((cols, rows, 3), dtype=np.uint8)
+    CLASS_NAMES = {0: "Linear", 1: "Poli", 2: "RBF", 3:"Sigmoid", 4:"Chi2", 5:"Inter"}
 
     for i in range(0, len(predictions)):
         if predictions[i] == 0:
@@ -45,6 +44,7 @@ def imageResult(predictions):
             data[int(np.floor(i / rows)), int(i % rows)] = [124, 252, 0]
 
     img = Image.fromarray(data)  # Create a PIL image
+    print(CLASS_NAMES[kernel] + str(c))
     img.show()
 
 
@@ -139,7 +139,8 @@ def calculateStatistics(posibleValues, confusionMatrix):
 
 
 def main():
-    mode = False
+    mode = True
+    cows = True
     trainingSet = [[], []]
     testSet = [[], []]
 
@@ -147,14 +148,16 @@ def main():
         cowSet = loadTrainingFile('data/images/vaca.jpg', 0)
         skySet = loadTrainingFile('data/images/cielo.jpg', 1)
         grassSet = loadTrainingFile('data/images/pasto.jpg', 2)
+        comb = cowSet + skySet + grassSet
+        random.shuffle(comb)
+        split = 0.6
+        trainingSet[0].extend(comb[:math.floor(len(comb) * split)])
+        testSet[0].extend(comb[math.floor(len(comb) * split):])
 
-        trainingSet[0] = cowSet + skySet + grassSet
-        testSet[0] = loadTrainingFile('data/images/cow.jpg', -1)
-        testSet[1] = loadTestingFileLabels('data/images/cow16.bmp')
         for row in trainingSet[0]:
             trainingSet[1].append(row.pop(-1))
         for row in testSet[0]:
-            row.pop(-1)
+            testSet[1].append(row.pop(-1))
     else:
         set = [[], []]
         set[0] = loadTrainingFile('data/images/cow.jpg', -1)
@@ -179,9 +182,11 @@ def main():
     trainingData = np.matrix(trainingSet[0], dtype=np.float32)
     trainingLabels = np.array(trainingSet[1])
 
-    for a in range(5,6):
+    CLASS_NAMES = {0: "Linear", 1: "Poli", 2: "RBF", 3:"Sigmoid", 4:"Chi2", 5:"Inter"}
+
+    for a in range(0,6):
         for c in [0.1, 0.5, 1, 5, 10]:
-            print(a)
+            print(CLASS_NAMES[a])
             print(c)
             svm = cv.ml.SVM_create()
             svm.setType(cv.ml.SVM_C_SVC)
@@ -201,8 +206,21 @@ def main():
             predictions = list(predictions.transpose()[0])  # Transform vertical nparray to list
 
             calculateConfusionMatrix(testSet[1], predictions, [0, 1, 2])
-            if not mode:
-                imageResult(predictions)
+
+            if cows:
+                cowPredict = loadTrainingFile('data/images/cow.jpg', -1)
+                for row in cowPredict:
+                    row.pop(-1)
+                testingData = np.matrix(cowPredict, dtype=np.float32)
+                predictions = svm.predict(testingData)[1]
+                imageResult(predictions, a, c, 1140, 760)
+
+                cowPredict2 = loadTrainingFile('data/images/vaca2.jpg', -1)
+                for row in cowPredict2:
+                    row.pop(-1)
+                testingData2 = np.matrix(cowPredict2, dtype=np.float32)
+                predictions2 = svm.predict(testingData2)[1]
+                imageResult(predictions2, a, c, 600, 360)
 
 
 main()
